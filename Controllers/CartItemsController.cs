2b1e0c6f3bb13da33a74b50c24599643a7496872
Pavo ┼áimi─ç <psimic@vub.hr>
2025-02-23 20:02:11 +0100

    Scaffoldan model cartitem

---
+++ Controllers/CartItemsController.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebShop.Data;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class CartItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CartItems
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.CartItems.Include(c => c.Cart).Include(c => c.Product);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: CartItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItems
+                .Include(c => c.Cart)
+                .Include(c => c.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Create
+        public IActionResult Create()
+        {
+            ViewData["CartId"] = new SelectList(_context.Carts, "Id", "Id");
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Name");
+            return View();
+        }
+
+        // POST: CartItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProductId,Quantity,CartId")] CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cartItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CartId"] = new SelectList(_context.Carts, "Id", "Id", cartItem.CartId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Name", cartItem.ProductId);
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItems.FindAsync(id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+            ViewData["CartId"] = new SelectList(_context.Carts, "Id", "Id", cartItem.CartId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Name", cartItem.ProductId);
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductId,Quantity,CartId")] CartItem cartItem)
+        {
+            if (id != cartItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cartItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartItemExists(cartItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CartId"] = new SelectList(_context.Carts, "Id", "Id", cartItem.CartId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Name", cartItem.ProductId);
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItems
+                .Include(c => c.Cart)
+                .Include(c => c.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cartItem = await _context.CartItems.FindAsync(id);
+            if (cartItem != null)
+            {
+                _context.CartItems.Remove(cartItem);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartItemExists(int id)
+        {
+            return _context.CartItems.Any(e => e.Id == id);
+        }
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebShop.Models;
 
 namespace WebShop.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
+        : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
 }
