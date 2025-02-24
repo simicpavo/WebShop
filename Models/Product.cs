6b5f364d0c96e17f767382f68bb367be8fad5dc5
Pavo ┼áimi─ç <psimic@vub.hr>
2025-02-24 16:25:45 +0100

    Promjena product modela

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,9 +12,11 @@ public class Product
     [Required]
     public decimal Price { get; set; }
 
-    public int CategoryId { get; set; }
-    public Category Category { get; set; }
+    public string? ImagePath { get; set; }
 
+    [Required]
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
     public int? DiscountId { get; set; }
-    public Discount Discount { get; set; }
+    public Discount? Discount { get; set; }
 }
