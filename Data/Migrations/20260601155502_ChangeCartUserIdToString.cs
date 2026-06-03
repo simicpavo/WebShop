5f2e3d8c4784b070d8170ba9b9addd89660a186d
simicpavo <pavsimic@gmail.com>
2026-06-03 12:48:03 +0200

    Change CartUserId from integer to string

---
+++ Data/Migrations/20260601155502_ChangeCartUserIdToString.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebShop.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCartUserIdToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_AspNetUsers_UserId1",
+                table: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_UserId1",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Carts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Carts",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_AspNetUsers_UserId",
+                table: "Carts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_AspNetUsers_UserId",
+                table: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Carts",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_UserId1",
+                table: "Carts",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_AspNetUsers_UserId1",
+                table: "Carts",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
