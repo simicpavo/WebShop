9f45f393f42a7fa9606f1df984e78db95213fd11
simicpavo <pavsimic@gmail.com>
2026-06-03 12:51:02 +0200

    Change CartID to string

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace WebShop.Models;
 
 public class Cart
 {
     public int Id { get; set; }
-    public int UserId { get; set; }
+    public string UserId { get; set; }
     public IdentityUser User { get; set; }
     public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
 }
