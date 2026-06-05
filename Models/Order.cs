dc3f6446419476a811f86f197df9acbacfb834fa
simicpavo <pavsimic@gmail.com>
2026-06-05 13:41:48 +0200

    Add models

---
+++ Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace WebShop.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new();
+    }
+}
